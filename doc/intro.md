56a4c67dad8ab0b7dabee3c2c5b08084e0d9867c
Shriphani Palakodety <shriphanip@gmail.com>
2014-06-13 14:40:01 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-kba
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clj_kba/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-kba.core-test
+  (:use clojure.test
+        clj-kba.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
