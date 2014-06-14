a77c43e854891caf36ceca88a8bccb82e1055d9e
Shriphani Palakodety <shriphanip@gmail.com>
2014-06-14 19:07:53 -0400

    runs on entire corpus now

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.apache.thrift/libthrift "0.9.1"]
-                 [org.clojure/clojure "1.4.0"]
-                 [ch.qos.logback/logback-classic "1.0.13"]]
-  :java-source-paths ["src/gen-java"])
+  :dependencies [[byte-streams "0.1.10"]
+                 [ch.qos.logback/logback-classic "1.0.13"]
+                 [cheshire "5.3.1"]
+                 [org.apache.thrift/libthrift "0.9.1"]
+                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.3.1"]]
+  :java-source-paths ["src/gen-java"]
+  :jvm-opts ["-Xmx10g"]
+  :main clj-kba.core)
--- src/clj_kba/core.clj
+++ src/clj_kba/core.clj
@@ -1,16 +1,25 @@
 (ns clj-kba.core
+  (:require [clojure.java.io :as io]
+            [clojure.tools.cli :refer [parse-opts]])
+  (:use [byte-streams]
+        [cheshire.core]
+        [clojure.pprint :only [pprint]]
+        [clojure.walk :only [keywordize-keys]])
   (:import [java.io BufferedInputStream File FileInputStream]
+           [java.nio.charset Charset]
            [java.util.zip GZIPInputStream]
-           [kba StreamItem StreamTime CorpusItem]
+           [kba ContentItem CorpusItem StreamItem]
            [org.apache.thrift TException]
            [org.apache.thrift.transport
             TTransport
             TIOStreamTransport
             TTransportException]
-           [org.apache.thrift.protocol TProtocol TBinaryProtocol]))
+           [org.apache.thrift.protocol TProtocol TBinaryProtocol TJSONProtocol]))
 
 (defn corpus-files
   [corpus-dir]
+  (println :dir-name corpus-dir)
+  (flush)
   (filter
    #(re-find #".gz$" (.getAbsolutePath %))
    (file-seq (File. corpus-dir))))
@@ -22,6 +31,20 @@
      #(re-find #"social\."(.getAbsolutePath %))
      files)))
 
+(defn stream-item-obj->map
+  "A stream item is equipped with bytes which when
+   read are lost. We fix that by building a map"
+  [a-stream-item]
+  (let [body (convert (.body a-stream-item)
+                      ContentItem)
+        source-meta-data (-> (.source_metadata a-stream-item)
+                             (convert String)
+                             parse-string
+                             keywordize-keys)]
+    {:meta source-meta-data
+     :body (convert (.raw body)
+                    String)}))
+
 (defn stream-items-seq
   [a-protocol]
   (take-while
@@ -30,17 +53,80 @@
     (fn []
       (let [a-stream-item (StreamItem.)]
         (do (try (do (.read a-stream-item a-protocol)
-                     (println a-stream-item)
-                     a-stream-item)
+                     (stream-item-obj->map a-stream-item))
                  (catch TTransportException e nil))))))))
 
 (defn read-file
+  "Reads the KBA streamcorpus file
+   builds a metadata and body map"
   [a-file]
   (let [transport (-> a-file
                       (FileInputStream.)
                       (GZIPInputStream.)
                       (TIOStreamTransport.))
-
+        
         protocol  (TBinaryProtocol. transport)]
     (do (.open transport)
         (stream-items-seq protocol))))
+
+(defn forum-items
+  [a-file]
+  (let [items (read-file a-file)]
+    (filter
+     (fn [item]
+       (= "Forum"
+          (-> item
+              :meta
+              :feed_class)))
+     items)))
+
+(defn non-4chan-forum-items
+  [a-file]
+  (let [items (forum-items a-file)]
+    (filter
+     (fn [item]
+       (not
+        (re-find #"4chan"
+                 (-> item
+                     :meta
+                     :home_link))))
+     items)))
+
+(defn most-recent-body-host-name
+  [a-file]
+  (let [cset (Charset/forName "UTF-8")]
+   (reduce
+    (fn [acc item]
+      (merge acc {(-> item :meta :home_link)
+                  (:body item)}))
+    {}
+    (non-4chan-forum-items a-file))))
+
+(defn corpus-most-recent-body-host-name
+  [corpus-dir]
+  (reduce
+   (fn [acc file]
+     (do (println :processing (.getAbsolutePath file))
+         (flush)
+         (let [to-merge (try (most-recent-body-host-name file)
+                             (catch Exception e (println
+                                                 (str "Fuck up file "
+                                                      (.getAbsolutePath file)))))]
+           (merge acc to-merge))))
+   {}
+   (corpus-social-files corpus-dir)))
+
+(defn dump-host-names-bodies
+  ([corpus-dir]
+     (dump-host-names-bodies corpus-dir ""))
+  ([corpus-dir prefix]
+     (let [to-dump-file (str prefix "-host-name-latest-body.clj")]
+       (println :dumping-to to-dump-file)
+       (with-open [wrtr (io/writer to-dump-file)]
+         (pprint (corpus-most-recent-body-host-name corpus-dir)
+                 wrtr)))))
+
+(defn -main
+  [& args]
+  (let [arguments (:arguments (parse-opts args []))]
+    (apply dump-host-names-bodies arguments)))
